5eca3e4f3f3a95ef6d95d9e043c6b2ef8bf37966
SlaMax <157590758+SlaMax@users.noreply.github.com>
2026-04-07 14:40:01 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const express = require('express');
-const path = require('path'); 
+const path = require('path');
+const fs = require('fs'); // Dodajemo 'fs' modul za čitanje datoteka
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public')); 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// Ruta za /slike koja čita iz JSON datoteke
+app.get('/slike', (req, res) => {
+    fs.readFile('slike.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error("Greška pri čitanju datoteke:", err);
+            return res.status(500).send("Greška na serveru");
+        }
+        const slikeIzJsona = JSON.parse(data);
+        
+        res.render('slike', { podaci: slikeIzJsona });
+    });
+});
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server pokrenut na portu ${PORT}`);
+    console.log(`Server radi na portu ${PORT}`);
 });
